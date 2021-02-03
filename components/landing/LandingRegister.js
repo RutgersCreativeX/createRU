f23846a6f7f71b94064d3261f5767943d6d312c0
Edwin Luo <edwinluo28@gmail.com>
2021-02-02 20:45:36 -0500

    button functionality

--- components/landing/LandingRegister.js
+++ components/landing/LandingRegister.js
@@ -1,10 +1,17 @@
 import styles from "./landing.module.css";
+import Link from "next/link";
 
 const LandingRegister = () => {
   return (
-    <div className={styles.landingRegister}>
-      <div className={styles.registerContainer}>REGISTER NOW</div>
-    </div>
+    <a
+      href="https://3613i9cyvrz.typeform.com/to/CHmHWmuh"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <div className={styles.landingRegister}>
+        <div className={styles.registerContainer}>REGISTER NOW</div>
+      </div>
+    </a>
   );
 };
 
